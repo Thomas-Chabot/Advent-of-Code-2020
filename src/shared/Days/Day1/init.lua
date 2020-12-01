01eeec410078b8d640ed0b172587f525fd2b6dcf
Sean Hodges <sean.hodges@nserc-crsng.gc.ca>
2020-12-01 12:51:09 -0500

    Part 2 Answer

--- src/shared/Days/Day1/init.lua
+++ src/shared/Days/Day1/init.lua
@@ -16,27 +16,29 @@
 
 type ArrayOfNumber = { number }
 type SumResultType = {
-    Left: {
-        Index: number,
-        Value: number?
-    },
-    Right: {
-        Index: number,
-        Value: number?
-    }
+    Index: number,
+    Value: number?
 }
 
 local Parser = require(game.ReplicatedStorage.Common.Library.ParseInput)
 local Input = Parser.ToNumbers(require(script.Input))
 
-function findSum(values: ArrayOfNumber, target: number): SumResultType
+-- Sorts an array of numbers
+local function sortValues(values: ArrayOfNumber)
+    table.sort(values, function(a, b) return a < b end)
+end
+
+-- Finds a pair totalling to a given target value.
+function findSum(values: ArrayOfNumber, target: number, startingIndex: number?, isSorted: boolean?): {SumResultType}?
     -- First step is to make sure that the array is sorted
     -- So that walking backwards/forwards will adjust our sum accordingly
-    table.sort(values, function(a, b) return a < b end)
+    if not isSorted then
+        sortValues(values)
+    end
 
     -- Set up our array indexers; these are values that point to the left and right of what we're looking at
     -- These will be adjusted as we walk through the array to find our target
-    local left : number = 1
+    local left : number = startingIndex or 1
     local right : number = #values
 
     -- Continue until we loop back on ourselves (left bypasses right)
@@ -60,29 +62,60 @@ function findSum(values: ArrayOfNumber, target: number): SumResultType
     -- If we found a sum, return them; otherwise indicate that we had no value
     if left <= right then
         return {
-            Left = {
+            {
                 Index = left,
                 Value = values[left]
             },
-            Right = {
+            {
                 Index = right,
                 Value = values[right]
             }
         }
     else
-        return {
-            Left = {
-                Index = -1
-            },
-            Right = {
-                Index = -1
-            }
-        }
+        return nil
     end
 end
 
-local day1Result = findSum(Input, 2020)
+-- Finds a triplet totalling to a given target value.
+local function findTriplet(values: ArrayOfNumber, target: number): {SumResultType}?
+    -- Step 1) Sort the array
+    sortValues(values)
+
+    -- Step 2) Walk through the array 1-by-1 to see if we can find a sum with which we can add the current value
+    --  If that's the case, then taking that sum + our value will give us our triplet
+    local i = 1
+    local result
+    while i < #values do
+        local targ = target - values[i]
+
+        -- Check if there's a sum that totals to the target
+        local foundResult = findSum(values, targ, i + 1, true)
+
+        -- If we have a result, add our current value into it & return that
+        if foundResult then
+            table.insert(foundResult, {
+                Index = i,
+                Value = values[i]
+            })
+
+            return foundResult
+        end
+
+        -- Otherwise continue pushing through the array
+        i += 1
+    end
+
+    -- At this point we found nothing, so return nil
+    return nil
+end
+
+local part1Result = findSum(Input, 2020)
+local part2Result = findTriplet(Input, 2020)
+
+local part1Answer = part1Result and part1Result[1].Value * part1Result[2].Value
+local part2Answer = part2Result and part2Result[1].Value * part2Result[2].Value * part2Result[3].Value
 
 return {
-    Day1Result = day1Result.Left.Value * day1Result.Right.Value
+    Part1Result = part1Answer,
+    Part2Result = part2Answer
 }
